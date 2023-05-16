150aba66bf0996d164ebfe3e79aa4115ef1e333f
DOMINICK DADDIO <dominick@crowdbotics.com>
2023-05-16 07:55:47 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "peconicpickleballmo-39384",
-  url: "https://peconicpickleballmo-39384.botics.co",
-  api: "https://peconicpickleballmo-39384.botics.co/api/v1"
-}
+  "name": "peconicpickleballmo-39384",
+  "url": "https://peconicpickleballmo-39384.botics.co",
+  "api": "https://peconicpickleballmo-39384.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
